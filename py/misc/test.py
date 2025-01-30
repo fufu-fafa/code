6e7325b037356c52b729617fa234fdc4f8ea205e
raurus <osintstuf@gmail.com>
2025-01-30 17:28:37 +0700

    Delete py/misc/test.py

--- py/misc/test.py
+++
@@ -1,9 +0,0 @@
-test1 = input("input yes or no: ")
-if test1 == "yes" or test1 == "no":
-    is_yes = test1 == "yes"
-    if is_yes:
-        print("yes")
-    else:
-        print("no")
-else:
-    print("d0ngo")
