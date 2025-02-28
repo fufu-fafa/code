f041ff21e5a9c8e724e6462c4f9c6afa566ecb12
n1993rs <osintstuf@gmail.com>
2025-02-28 09:08:53 +0700

    1

--- py/misc/bin.py
+++ py/misc/bin.py
@@ -1,5 +1,13 @@
 import argparse
 
+def file_check(output_file):
+    try:
+        with open(output_file, 'r') as check:
+            check.read()
+            return True
+    except FileNotFoundError:
+        return False
+
 def text_to_binary(text):
     return ' '.join(format(ord(char), '08b') for char in text)
 
@@ -10,18 +18,34 @@ def binary_to_text(binary):
         return "Invalid binary input. Ensure each binary chunk is 8 bits."
 
 def encode_file(input_file, output_file):
-    with open(input_file, 'r', encoding='utf-8') as f:
-        text = f.read()
-    binary_data = text_to_binary(text)
-    with open(output_file, 'w', encoding='utf-8') as f:
-        f.write(binary_data)
+    if file_check(output_file):
+        print(f"error: {output_file} already exist")
+        return
+    try:
+        with open(input_file, 'r', encoding='utf-8') as f:
+            text = f.read()
+        binary_data = text_to_binary(text)
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.write(binary_data)
+    except FileNotFoundError:
+        print(f"error: {input_file} not found")
+    except Exception as e:
+        print(f"error: {e}")
 
 def decode_file(input_file, output_file):
-    with open(input_file, 'r', encoding='utf-8') as f:
-        binary_data = f.read()
-    text = binary_to_text(binary_data)
-    with open(output_file, 'w', encoding='utf-8') as f:
-        f.write(text)
+    if file_check(output_file):
+        print(f"error: {output_file} already exist")
+        return
+    try:
+        with open(input_file, 'r', encoding='utf-8') as f:
+            binary_data = f.read()
+        text = binary_to_text(binary_data)
+        with open(output_file, 'w', encoding='utf-8') as f:
+            f.write(text)
+    except FileNotFoundError:
+        print(f"error: {input_file} not found")
+    except Exception as e:
+        print(f"error: {e}")
 
 def main():
     parser = argparse.ArgumentParser(description="Encode or decode text using binary.")
@@ -29,12 +53,11 @@ def main():
     parser.add_argument("-d", "--decode", action="store_true", help="Decode binary to text.")
     parser.add_argument("-f", "--file", help="Input file to encode or decode.")
     parser.add_argument("-o", "--output", help="Output file to save the result.")
-    
     args = parser.parse_args()
     
     if args.file and args.output:
         if args.encode:
-            encode_file(args.file, args.output)
+           encode_file(args.file, args.output)
         elif args.decode:
             decode_file(args.file, args.output)
         else:
