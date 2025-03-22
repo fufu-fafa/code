678ab2b36c1ba1605d7a2283319b4e91d09ccc66
raurus <fufu-fafa@users.noreply.github.com>
2025-03-22 21:13:17 +0700

    auto-commit

--- java/misc/httpServer.java
+++ java/misc/httpServer.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.nio.file.Path;
 import java.net.InetSocketAddress;
 
 public class httpServer {
@@ -21,13 +22,41 @@ public class httpServer {
 class StaticFileHandler implements HttpHandler {
     @Override
     public void handle(HttpExchange exchange) throws IOException {
-        String filePath = "index.html";
-        byte[] fileBytes = Files.readAllBytes(Paths.get(filePath));
-        
-        exchange.sendResponseHeaders(200, fileBytes.length);
-        OutputStream os = exchange.getResponseBody();
-        os.write(fileBytes);
-        os.close();
+        System.out.println("Received request: " + exchange.getRequestMethod() + " " + exchange.getRequestURI());
+        String uriPath = exchange.getRequestURI().getPath();
+        if (uriPath.equals("/")) {
+            uriPath = "/index.html"; 
+        }
+
+        if (!uriPath.contains(".")) {
+            uriPath += ".html";
+        }
+
+        String filePath = "." + uriPath; // serve files from current directory
+        Path path = Paths.get(filePath);
+
+        if (Files.exists(path) && !Files.isDirectory(path)) {
+            byte[] fileBytes = Files.readAllBytes(path);
+
+            String contentType = "text/html";
+            if (filePath.endsWith(".css")) contentType = "text/css";
+            else if (filePath.endsWith(".js")) contentType = "application/javascript";
+            else if (filePath.endsWith(".png")) contentType = "image/png";
+            else if (filePath.endsWith(".jpg") || filePath.endsWith(".jpeg")) contentType = "image/jpeg";
+
+            exchange.getResponseHeaders().set("Content-Type", contentType);
+            exchange.sendResponseHeaders(200, fileBytes.length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(fileBytes);
+            os.close();
+        } else {
+            // Return 404 Not Found
+            String response = "404 (Not Found)\n";
+            exchange.sendResponseHeaders(404, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        }
     }
 }
 
