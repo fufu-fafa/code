acbcf29b196e12ded37195b1e614b5a160687bcd
raurus <fufu-fafa@users.noreply.github.com>
2025-03-22 20:49:57 +0700

    auto-commit

--- c/misc/calc.cpp
+++ c/misc/calc.cpp
@@ -4,11 +4,25 @@ int main() {
     double num1, num2;
     char op;
     cout << "enter first number: ";
-    cin >> num1;
+    if (!(cin >> num1)) {
+        cout << "err: invalid number" << endl;
+        return 1;
+    }
     cout << "enter an operator (+, -, *, /): ";
     cin >> op;
+    if (op != '+' && op != '-' && op != '*' && op != '/') {
+        cout << "err: invalid operator" << endl;
+        return 1;
+    }
     cout << "enter second number: ";
-    cin >> num2;
+    if (!(cin >> num2)) {
+        cout << "err: invalid number" << endl;
+        return 1;
+    }
+    if (op == '/' && num2 == 0) {
+        cout << "err: division by zero" << endl;
+        return 1;
+    }
     switch (op) {
         case '+':
             cout << "result: " << num1 + num2 << endl;
@@ -20,13 +34,8 @@ int main() {
             cout << "result: " << num1 * num2 << endl;
             break;
         case '/':
-            if (num2 != 0)
-                cout << "result: " << num1 / num2 << endl;
-            else
-                cout << "err: division by zero" << endl;
+            cout << "result: " << num1 / num2 << endl;
             break;
-        default:
-            cout << "invalid operator" << endl;
     }
     return 0;
 }
