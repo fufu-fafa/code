b01de94ad3af3534856fc7b1179b7a3daed50965
raurus <fufu-fafa@users.noreply.github.com>
2025-03-26 18:46:56 +0700

    auto-commit

--- learn_py/temperature_converter.py
+++ learn_py/temperature_converter.py
@@ -1,4 +1,21 @@
-val_unit = ["celcius", "fahrenheit", "kelvin"]
+def convert_unit(input_unit, first_unit, second_unit):
+    if first_unit == "celsius":
+        if second_unit == "fahrenheit":
+            return input_unit * 9/5 + 32
+        else:
+            return input_unit + 273.15
+    elif first_unit == "fahrenheit":
+        if second_unit == "celsius":
+            return (input_unit - 32) * 5/9
+        else:
+            return (input_unit - 32) * 5/9 + 273.15
+    else:
+        if second_unit == "celsius":
+            return input_unit - 273.15
+        else:
+            return (input_unit - 273.15) * 9/5 + 32
+
+val_unit = ["celsius", "fahrenheit", "kelvin"]
 print(f"valid units: {val_unit}")
 while True:
     first_unit = input("the unit of the input: ").lower().strip()
@@ -6,8 +23,11 @@ while True:
     if first_unit not in val_unit or second_unit not in val_unit:
         print("invalid unit")
         continue
+    elif first_unit == second_unit:
+        print("whatchu tryna do")
+        continue
     try:
-        input_unit = int(input("input the measurement: "))
+        input_unit = float(input("input the measurement: "))
         result = convert_unit(input_unit, first_unit, second_unit)
         print(f"{second_unit}: {result}")
         break
