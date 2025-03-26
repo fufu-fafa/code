727bf2111a5aecb73d98e6eddd7086a13f1b973e
raurus <fufu-fafa@users.noreply.github.com>
2025-03-26 18:48:18 +0700

    auto-commit

--- learn_py/temperature_converter.py
+++ learn_py/temperature_converter.py
@@ -19,8 +19,11 @@ val_unit = ["celsius", "fahrenheit", "kelvin"]
 print(f"valid units: {val_unit}")
 while True:
     first_unit = input("the unit of the input: ").lower().strip()
+    if first_unit not in val_unit:
+        print("invalid unit")
+        continue
     second_unit = input("the unit to convert into: ").lower().strip()
-    if first_unit not in val_unit or second_unit not in val_unit:
+    if second_unit not in val_unit:
         print("invalid unit")
         continue
     elif first_unit == second_unit:
