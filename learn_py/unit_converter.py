54bd5ddc287813d47fefa2bed079d849e1448128
raurus <fufu-fafa@users.noreply.github.com>
2025-03-27 21:42:21 +0700

    auto-commit

---
+++ learn_py/unit_converter.py
@@ -0,0 +1,28 @@
+valid_units = ["kilometer", "meter", "centimeter", "miles", "foot", "inch"]
+eu_units = ["kilometer", "meter", "centimeter"]
+
+def convert_unit(first_unit, second_unit, unit_value):
+    conversion_factors = {"kilometer": 1000, "meter": 1, "centimeter": 0.01,
+"miles": 1603.34, "foot": 0.3048, "inch": 0.0254}
+
+def validate_unit(in_unit):
+    if in_unit not in valid_units:
+        print("invalid unit")
+        print(f"valid units: {valid_units}")
+        return False
+
+def get_int(first_unit):
+    try:
+        return float(input(f"enter the value in {first unit}"))
+    except ValueError:
+        print("invalid number")
+
+print(f"valid units: {valid_units}")
+while True:
+    first_unit = input("enter the unit to convert from: ").lower().strip()
+    if not validate_unit(first_unit):
+        continue
+    second_unit = input("enter the unit to convert into: ").lower().strip()
+    if not validate_unit(second_unit):
+        continue
+    unit_value = get_int(first_unit)
