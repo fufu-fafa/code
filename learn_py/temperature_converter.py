8dfe3bf54ef840ec3a7df93ddc54c0c12e171565
raurus <fufu-fafa@users.noreply.github.com>
2025-03-27 22:33:34 +0700

    auto-commit

--- learn_py/temperature_converter.py
+++ learn_py/temperature_converter.py
@@ -18,14 +18,18 @@ def convert_unit(input_unit, first_unit, second_unit):
             return (input_unit - 273.15) * 9/5 + 32
 
 val_unit = ["celsius", "fahrenheit", "kelvin"]
-print_color(f"valid units: {val_unit}", "red")
+print_color(f"valid units: {val_unit}", "green")
 while True:
     first_unit = input("the unit of the input: ").lower().strip()
-    if first_unit not in val_unit:
+    if first_unit == "exit":
+        break
+    elif first_unit not in val_unit:
         print_color("invalid unit", "red")
         continue
     second_unit = input("the unit to convert into: ").lower().strip()
-    if second_unit not in val_unit:
+    if second_unit == "exit":
+        break
+    elif second_unit not in val_unit:
         print_color("invalid unit", "red")
         continue
     elif first_unit == second_unit:
