70677cf9eb6addfd27c029822807d2ca2253db05
raurus <fufu-fafa@users.noreply.github.com>
2025-03-27 22:19:57 +0700

    auto-commit

--- learn_py/unit_converter.py
+++ learn_py/unit_converter.py
@@ -1,19 +1,22 @@
+from cust_col import print_color
+
 valid_units = ["kilometer", "meter", "centimeter", "miles", "foot", "inch"]
 
 def convert_unit(first_unit, second_unit, unit_value):
     conversion_factors = {"kilometer": 1000, "meter": 1, "centimeter": 0.01,
-        "miles": 1603.34, "foot": 0.3048, "inch": 0.0254}
+        "miles": 1609.34, "foot": 0.3048, "inch": 0.0254}
     value_in_meters = unit_value * conversion_factors[first_unit]
     result = value_in_meters / conversion_factors[second_unit]
     return result
 
-def validate_unit(in_unit):
-    if in_unit in valid_units:
-        return True
-    else:
-        print("invalid unit")
-        print(f"valid units: {valid_units}")
-        return False
+def validate_unit(which_unit):
+    prompt = "enter the unit to convert from: " if which_unit == "first" else "enter the unit to convert into"
+    while True:
+        temp = input(prompt).lower().strip()
+        if temp not in valid_units:
+            print_color("invalid unit", "red")
+            continue
+        return temp
 
 def get_int(first_unit):
     try:
@@ -21,17 +24,9 @@ def get_int(first_unit):
     except ValueError:
         print("invalid number")
 
-print(f"valid units: {valid_units}")
+print_color(f"valid units: {valid_units}", "green")
 while True:
-    first_unit = input("enter the unit to convert from: ").lower().strip()
-    if first_unit == "exit":
-        break
-    elif not validate_unit(first_unit):
-        continue
-    second_unit = input("enter the unit to convert into: ").lower().strip()
-    if second_unit == "exit":
-        break
-    elif not validate_unit(second_unit):
-        continue
+    first_unit = validate_unit("first")
+    second_unit = validate_unit("second")
     unit_value = get_int(first_unit)
     print(f"{convert_unit(first_unit, second_unit, unit_value)} {second_unit}")
