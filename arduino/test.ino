817d16d008ba76bf7413de96db657417bf432347
raurus <fufu-fafa@users.noreply.github.com>
2025-03-28 20:59:22 +0700

    auto-commit

---
+++ arduino/test.ino
@@ -0,0 +1,9 @@
+void setup() {
+    for (int pin = 2; pin <= 9; pin++) {
+        pinMode(pin, OUTPUT);
+    }
+    for (int pin = 2; pin <= 9; pin++) {
+        digitalWrite(pin, HIGH)
+        delay(1000)
+    }
+}
