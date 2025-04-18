0da52e990900d35f71904b7f59e706a7a63e05ae
raurus <fufu-fafa@users.noreply.github.com>
2025-04-18 17:50:24 +0700

    auto-commit

--- esp32/pwm5/pwm5.ino
+++ esp32/pwm5/pwm5.ino
@@ -1,14 +1,25 @@
+void pwm_s1(int pin1, int pin2) {
+    for (int x = 0; x <= 255; x++) {
+        analogWrite(pin1, x);
+        analogWrite(pin2, 255 - x);
+        delay(10);
+    }
+}
+
+void pwm_s2(int pin1, int pin2) {
+    for (int x = 255; x >= 0; x--) {
+        analogWrite(pin1, x);
+        analogWrite(pin2, 255 - x);
+        delay(10);
+    }
+}
+
 void setup() {
     pinMode(5, OUTPUT);
+    pinMode(16, OUTPUT);
 }
 
 void loop() {
-    for (int sybau = 0; sybau <= 255; sybau++) {
-        analogWrite(5, sybau);
-        delay(15);
-    }
-    for (int sybau = 255; sybau >= 0; sybau--) {
-        analogWrite(5, sybau);
-        delay(15);
-    }
+    pwm_s1(5, 16);
+    pwm_s2(5, 16);
 }
