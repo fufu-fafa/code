43798d593f5b55abb484f194f14c573fa3258e7f
raurus <fufu-fafa@users.noreply.github.com>
2025-04-22 10:09:23 +0700

    auto-commit

--- learn_java/Main.java
+++ learn_java/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Inventory inventory = new Inventory();
         Item item1 = new Item("nice people", 20);
-        Item item2 = new Item("bitches", 1);
+        Item item2 = new Item("y", 1);
         inventory.addItem(item1);
         inventory.addItem(item2);
         inventory.displayInventory();
