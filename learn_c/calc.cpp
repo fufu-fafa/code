1aef096cd0b227d411b99afe9ea7436b5af2b422
raurus <fufu-fafa@users.noreply.github.com>
2025-04-23 21:38:55 +0700

    auto-commit

--- learn_c/calc.cpp
+++ learn_c/calc.cpp
@@ -2,17 +2,44 @@
 #include <string>
 using namespace std;
 
+float get_float(string var) {
+    float temp;
+    while (true) {
+        cout << "enter the " << var << " number: ";
+        cin >> temp;
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            cout << "invalid number" << endl;
+        } else {
+            break;
+        }
+    }
+    return temp;
+}
+
+float calculate(float num1, float num2) {
+
+}
+
 int main() {
-    double num1, num2, result;
+    float num1, num2, result;
     string chosen_op;
-    cout << "input the first number: ";
-    cin >> num1;
+    num1 = get_float("first");
 
     cout << "input one of the valid operator: <+, -, *, /> ";
     cin >> chosen_op;
 
-    cout << "input the second number: ";
-    cin >> num2;
+    if (chosen_op != "+" && chosen_op != "-" && chosen_op != "*" && chosen_op != "/") {
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "invalid operator" << endl;
+        cout << "input one of the valid operator: <+, -, *, /> "
+        cin >> chosen_op;
+    }
+
+    num2 = get_float("second");
+
     if (chosen_op == "+") {
         result = num1 + num2;
         cout << "result: " << result << endl;
