f9b52a4aff83e1fc2bbdf99afb9c8c27842ee79e
raurus <190369371+fufu-fafa@users.noreply.github.com>
2025-05-02 21:55:28 +0700

    auto-commit

--- learn_c/temperature_converter.cpp
+++ learn_c/temperature_converter.cpp
@@ -21,13 +21,31 @@ std::string get_temperature(std::string which) {
     while (true) {
         std::cout << "enter the temperature to " << which << ": ";
         std::cin >> temperature;
-        if (temperature == "celcius" || temperature == "fahrenheit" || temperature == "kelvin") return temperature;
+        if (temperature == "celsius" || temperature == "fahrenheit" || temperature == "kelvin") return temperature;
         else {
             std::cin.ignore(100000, '\n');
             std::cout << "not a valid temperature (case sensitive)" << '\n'
-                      << "valid temperature: celcius, fahrenheit, kelvin" << '\n';
+                      << "valid temperature: celsius, fahrenheit, kelvin" << '\n';
         }
     }
+}
+
+double calculate(std::string first_temperature, std::string second_temperature, double input_measurement) {
+    double output_measurement;
+    if (first_temperature == "celsius") {
+        if (second_temperature == "fahrenheit") output_measurement = input_measurement * 9.0/5.0 + 32;
+        else if (second_temperature == "kelvin") output_measurement = input_measurement + 273.15;
+        else output_measurement = input_measurement;
+    } else if (first_temperature == "fahrenheit") {
+        if (second_temperature == "celsius") output_measurement = (input_measurement - 32) * 5.0/9.0;
+        else if (second_temperature == "kelvin") output_measurement = (input_measurement - 32) * 5.0/9.0 + 273.15;
+        else output_measurement = input_measurement;
+    } else {
+        if (second_temperature == "celsius") output_measurement = input_measurement - 273.15;
+        else if (second_temperature == "fahrenheit") output_measurement = (input_measurement - 273.15) * 9.0/5.0 + 32;
+        else output_measurement = input_measurement;
+    }
+    return output_measurement;
 }
 
 int main() {
@@ -37,5 +55,7 @@ int main() {
     second_temperature = get_temperature("convert into");
 
     input_measurement = get_num(first_temperature);
+    output_measurement = calculate(first_temperature, second_temperature, input_measurement);
+    std::cout << "result: " << output_measurement << '\n';
     return 0;
 }
