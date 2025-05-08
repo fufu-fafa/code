472ba6e0e28f7a803a86a560be56d23f54ff88c5
raurus <190369371+fufu-fafa@users.noreply.github.com>
2025-05-08 19:54:22 +0700

    auto-commit

---
+++ learn_c/line_equation.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <limits>
+
+struct coordinate {
+    double x, y;
+};
+
+struct line {
+    coordinate start, end;
+    double gradient, offset;
+};
+
+double get_gradient(coordinate start, coordinate end) {
+    if (end.x == start.x) return std::numeric_limits<double>::infinity();
+    else return (end.y - start.y) / (end.x - start.x);
+}
+
+coordinate get_coordinate() {
+    coordinate temp;
+    while (true) {
+        std::cin >> temp.x >> temp.y;
+        if (std::cin.fail()) {
+            std::cout << "invalid number, reinput the coordinate (format: x y): " >> '\n';
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max, '\n'); 
+        } else {
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max, '\n');
+            return temp
+        }
+    }
+}
+
+int main() {
+    line line1
+    std::cout << "enter the first coordinate for the line: " << '\n';
+    line1.start = get_coordinate();
+    std::cout << "enter the second coordinate for the line: " << '\n';
+    line1.end = get_coordinate();
+    line1.gradient = get_gradient();
+    line1.offset = line1.start.y - line1.gradient * line1.start.x;
+
+    if (line1.offset > 0) std::cout << "y = " << line1.gradient << "x +" << line1.offset << '\n';
+    else if (line1.offset = 0) std::cout << "y = " << line1.gradient << '\n';
+    else std::cout << "y = " << line1.gradient << "x " << line1.offset << '\n';
+}
