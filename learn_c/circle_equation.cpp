7c004a33660926993370f52fafdc67aed6822f46
raurus <190369371+fufu-fafa@users.noreply.github.com>
2025-05-09 14:48:00 +0700

    auto-commit

--- learn_c/circle_equation.cpp
+++ learn_c/circle_equation.cpp
@@ -31,7 +31,7 @@ coordinate get_coordinate() {
     while (true) {
         std::cin >> temp.x >> temp.y;
         if (std::cin.fail()) {
-            std::cout << "invalid number, reinput the coordinate (format: x y): " << '\n';
+            std::cout << "invalid number, reinput the coordinate (format: x y):" << '\n';
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         } else {
@@ -43,21 +43,47 @@ coordinate get_coordinate() {
 
 circle option1() {
     circle temp;
-    std::cout << "enter the center point coordinate (format: x y): " << '\n';
+    std::cout << "enter the center point coordinate (format: x y):" << '\n';
     temp.center = get_coordinate();
-    std::cout << "enter the radius of the circle: ";
+    std::cout << "enter the radius of the circle:";
     temp.r = get_num();
     return temp;
 }
 
-circle option2() {}
+circle option2() {
+    circle temp;
+    coordinate p1, p2, p3;
+    double determinant, p1_sq, p2_sq, p3_sq;
+
+    std::cout << "enter the first point (format: x y):" << '\n';
+    p1 = get_coordinate();
+    std::cout << "enter the second point (format: x y):" << '\n';
+    p2 = get_coordinate();
+    std::cout << "enter the third point (format: x y):" << '\n';
+    p3 = get_coordinate();
+
+    determinant = 2 * (p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p1.y));
+    if (determinant == 0) {
+        std::cout << "points are collinear, cannot form a circle" << '\n';
+        exit(1);
+    }
+
+    p1_sq = std::pow(p1.x, 2) + std::pow(p1.y, 2);
+    p2_sq = std::pow(p2.x, 2) + std::pow(p2.y, 2);
+    p3_sq = std::pow(p3.x, 2) + std::pow(p3.y, 2);
+
+    temp.center.x = (p1_sq * (p2.y - p3.y) + p2_sq * (p3.y - p1.y) + p3_sq * (p1.y - p2.y)) / determinant;
+    temp.center.y = (p1_sq * (p3.x - p2.x) + p2_sq * (p1.x - p3.x) + p3_sq * (p2.x - p3.x)) / determinant;
+    temp.r = std::sqrt(std::pow(p1.x - temp.center.x, 2) + std::pow(p1.y - temp.center.y, 2));
+    return temp;
+}
 
 circle option3() {
     circle temp;
     coordinate edge;
-    std::cout << "enter the center point coordinate (format: x y): " << '\n';
+    std::cout << "enter the center point coordinate (format: x y):" << '\n';
     temp.center = get_coordinate();
-    std::cout << "enter the coordinate of a point on the circle: ";
+    std::cout << "enter the coordinate of a point on the circle:";
     edge = get_coordinate();
 
     if (temp.center.x == edge.x && temp.center.y == edge.y) {
@@ -65,20 +91,20 @@ circle option3() {
         exit(1);
     }
 
-    temp.r = sqrt(pow(edge.x - temp.center.x, 2) + pow(edge.y - temp.center.y, 2));
+    temp.r = sqrt(std::pow(edge.x - temp.center.x, 2) + std::pow(edge.y - temp.center.y, 2));
     return temp;
 }
 
 circle option4() {
     circle temp;
     coordinate endpoint1, endpoint2;
-    std::cout << "enter the first diameter endpoint (format: x y): " << '\n';
+    std::cout << "enter the first diameter endpoint (format: x y):" << '\n';
     endpoint1 = get_coordinate();
-    std::cout << "enter the second diameter endpoint (format: x y): " << '\n';
+    std::cout << "enter the second diameter endpoint (format: x y):" << '\n';
     endpoint2 = get_coordinate();
     temp.center.x = (endpoint1.x + endpoint2.x) / 2;
     temp.center.y = (endpoint1.y + endpoint2.y) / 2;
-    temp.r = sqrt(pow(endpoint2.x - endpoint1.x, 2) + pow(endpoint2.y - endpoint1.y, 2)) / 2;
+    temp.r = sqrt(std::pow(endpoint2.x - endpoint1.x, 2) + std::pow(endpoint2.y - endpoint1.y, 2)) / 2;
     return temp;
 }
 
