f26e048630d79853c9e8867eaaae409235c720cb
raurus <190369371+fufu-fafa@users.noreply.github.com>
2025-05-13 17:43:20 +0700

    auto-commit

---
+++ esp32/pwm_bt/pwm_bt.ino
@@ -0,0 +1,34 @@
+#include "BluetoothSerial.h"
+
+BluetoothSerial SerialBT;
+int len = 7;
+
+void setup() {
+    pinMode(25, OUTPUT);
+    Serial.begin(115200);
+    SerialBT.begin("esp32_pwm");
+    Serial.println("bluetooth is active, connect and send a number for 'len'");
+}
+
+void loop() {
+    if (SerialBT.available()) {
+        String input = SerialBT.readStringUntil('\n');
+        int newLen = input.toInt();
+        if (newLen > 0 && newLen <= 1000) {  // limit to a reasonable range
+            len = newLen;
+            SerialBT.println("updated delay to: " + String(len));
+        } else {
+            SerialBT.println("invalid input, enter an integer between 1 and 1000.");
+        }
+    }
+
+    for (int x = 0; x <= 255; x++) {
+        analogWrite(25, x);
+        delay(len);
+    }
+
+    for (int y = 255; y >= 0; y--) {
+        analogWrite(25, y);
+        delay(len);
+    }
+}
