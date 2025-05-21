e7cee7311c142a0408fc5ada6d3230420b26c6c9
raurus <190369371+fufu-fafa@users.noreply.github.com>
2025-05-21 19:52:23 +0700

    auto-commit

---
+++ c/learn/parabolic_equation.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <sstream>
+#include <limits>
+#include <cmath>
+
+struct coordinate {
+    double x, y;
+}
+
+struct parabole {
+    double p;
+    coordinate focus_point;
+}
+
+std::string format_double(double value) {
+    std::ostringstream temp;
+
+    if (value < 0) temp << "- " << -value;
+    else temp << "+ " << value;
+    return temp.str();
+}
+
+double get_num() {
+    double temp;
+
+    while (true) {
+        std::cin >> temp;
+        if (std::cin.fail()) {
+            std::cout << "invalid input, reinput the number: ";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
+        } else {
+            std::cin.ignore(std::numeric_limits<streamsize>::max(), '\n');
+            return temp;
+        }
+    }
+}
+
+double get_coordinate() {
+    coordinate temp;
+
+    while (true) {
+        std::cin >> temp.x >> temp.y;
+        if (std::cin.fail()) {
+            std::cout << "invalid input, reinput the coordinate (format: x y): " << '\n';
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        } else {
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            return temp;
+        }
+    }
+}
+
+int main() {
+    parabole result;
+}
