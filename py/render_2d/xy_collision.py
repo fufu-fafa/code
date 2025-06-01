d29332b49623620c6242e9868bb2b5a5b3eb0f88
raurus <190369371+fufu-fafa@users.noreply.github.com>
2025-06-01 12:06:05 +0700

    auto-commit

--- py/render_2d/xy_collision.py
+++ py/render_2d/xy_collision.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from matplotlib.animation import FuncAnimation
 import numpy as np
+import random
 
 # setup
 fig, ax = plt.subplots()
@@ -10,28 +11,37 @@ ax.set_xlim(-20, 20)
 ax.set_ylim(-20, 20)
 ax.axis('on')
 
-circle1_pos = [4.0, 4.0]
-circle1_vel = [-1.0, -1.0]
+circle1_pos = [random.randint(0, 18), random.randint(0, 18)]
+circle1_vel = [random.random(), random.random()]
 
-circle2_pos = [-4.0, -4.0]
-circle2_vel = [1.0, 1.0]
+circle2_pos = [random.randint(-18, 0), random.randint(-18, 0)]
+circle2_vel = [random.random(), random.random()]
 
-circle1 = patches.Circle((circle1_pos[0], circle1_pos[1]), 1, color='blue', alpha=0.6)
-circle2 = patches.Circle((circle2_pos[0], circle2_pos[1]), 1, color='red', alpha=0.6)
+circle1 = patches.Circle((circle1_pos[0], circle1_pos[1]), 2.0, color='blue', alpha=0.6)
+circle2 = patches.Circle((circle2_pos[0], circle2_pos[1]), 2.0, color='red', alpha=0.6)
 
 circles = [circle1, circle2]
 ax.add_patch(circle1)
 ax.add_patch(circle2)
 
+def check_circle_collision(pos1, pos2, rad=2.0):
+    distance = ((pos1[0] - pos2[0])**2 + (pos1[1] - pos2[1])) ** 0.5
+    if distance <= 2 * rad:
+        return True
+    else:
+        return False
+
 def check_handle_wall_collision(pos, vel):
     for n in range(2):
-        if pos[n] <= -19 or pos[n] >= 19:
+        if pos[n] <= -18 or pos[n] >= 18:
             vel[n] = vel[n] * -1
     return vel
 
-
 def update(frame):
     global circle1_pos, circle1_vel, circle2_pos, circle2_vel
+
+    if check_circle_collision(circle1_pos, circle2_pos):
+        circle1_vel, circle2_vel = circle2_vel, circle1_vel
 
     circle1_vel = check_handle_wall_collision(circle1_pos, circle1_vel)
     circle2_vel = check_handle_wall_collision(circle2_pos, circle2_vel)
