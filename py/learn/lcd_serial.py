9eb64baf1d2da98390c506118ca35ad37223bcc6
raurus <190369371+fufu-fafa@users.noreply.github.com>
2025-06-09 17:39:22 +0700

    auto-commit

---
+++ py/learn/lcd_serial.py
@@ -0,0 +1,16 @@
+import serial
+
+port = '/dev/tty.usbserial-0001'
+baud = 115200
+
+serial_esp32 = serial.Serial(port, baud, timeout=1)
+
+try:
+    while True:
+        message = input("send message to esp32: ").strip()
+        if message:
+            send = message.encode('utf-8')
+            serial_esp32.write(send)
+except KeyboardInterrupt:
+    print("\nexiting..")
+    serial_esp32.close()
