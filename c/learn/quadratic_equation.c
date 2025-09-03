84bab663de94df10c994107f74e7018b6626e8c1
raurus <190369371+fufu-fafa@users.noreply.github.com>
2025-09-03 20:29:55 +0700

    auto-commit

--- c/learn/quadratic_equation.c
+++ c/learn/quadratic_equation.c
@@ -7,7 +7,8 @@ void clearStdin() {
 }
 
 int isInvalid(double p1[2], double p2[2]) {
-    return ((p1[0] == p2[0]) && (p1[1] == p2[1]));
+    if (p1[0] == p2[0]) return 2;
+    else return ((p1[0] == p2[0]) && (p1[1] == p2[1]));
 }
 
 double det3(double m[3][3]) {
@@ -63,16 +64,19 @@ void getEquation(double points[3][2], double result[3]) {
 }
 
 int main() {
+    int inval;
     double p[3][2];
     double res[3];
     for (int n = 0; n < 3; n++) {
-        printf("enter the coordinate for point %d (format: x y):", n+1);
+        printf("enter the coordinate for point %d (format: x y): ", n+1);
         getCoord(p[n]);
     }
     for (int x = 0; x < 3; x++) {
         for (int y = x+1; y < 3; y++) {
-            if (isInvalid(p[x], p[y])) {
-                printf("any two points must be different\n");
+            inval = isInvalid(p[x], p[y]);
+            if (inval) {
+                if (inval == 1) printf("any two points must be different\n");
+                if (inval == 2) printf("any two points must have different x coordinate\n");
                 exit(1);
             }
         }
